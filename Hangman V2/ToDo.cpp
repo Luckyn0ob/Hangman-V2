d374a8e66a1a1a32af1f0161b19206030f947268
Timo Rogausch <XxAnubis15xX@aol.com>
2022-08-14 12:38:39 +0200

    fixed bug after new try would hangman painted finished

---
+++ Hangman V2/ToDo.cpp
@@ -0,0 +1,36 @@
+// ToDo list
+
+
+
+
+
+
+//BUGS
+//--------------------------------------------------------------------------------
+// 
+//exit after new try is not functionable
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// GAME IDEA
+//---------------------------------------------------------------------------------
+//
+// not shure if i wanna add function to call already guessed letter as mistake
+// wanna try to refresh 
